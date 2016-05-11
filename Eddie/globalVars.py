aaef7d9441e3d0e0c42397277920ba287a9e979f
Marien Wolthuis <marien@marienwolthuis.nl>
2016-05-11 18:57:13 +0200

    Added jointData channel and exit codes for Actions

--- Eddie/globalVars.py
+++ Eddie/globalVars.py
@@ -10,6 +10,7 @@ CHANNEL_MOVEMENTDATA= "tcp://127.0.0.1:4003"
 CHANNEL_ENERGYDATA  = "tcp://127.0.0.1:4004"
 CHANNEL_MODE        = "tcp://127.0.0.1:4005"
 CHANNEL_BPM         = "tcp://127.0.0.1:4006"
+CHANNEL_JOINTDATA   = "tcp://127.0.0.1:4007"
 
 ## Arduinos
 BOT_ARDUINO_ADDRESS = '/dev/ttyACM3'
@@ -20,3 +21,15 @@ IMU_ARDUINO_BAUDRATE= 115200
 
 ## Camera
 CAMERA_ID_NUMBER = 1
+
+## Exit codes for the Actions
+EXIT_CODE_DONE  = 0
+EXIT_CODE_ERROR = -1
+
+# Continue to a different Action:
+EXIT_CODE_A1    = 1
+EXIT_CODE_A2    = 2
+EXIT_CODE_A3    = 3
+EXIT_CODE_A4    = 4
+EXIT_CODE_A5    = 5
+
