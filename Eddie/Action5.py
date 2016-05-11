cb6aaf4e6c314a8b1b946c90682bc751a9c45c3e
Marien Wolthuis <marien@marienwolthuis.nl>
2016-05-11 21:33:11 +0200

    Added dummy Action subclasses for testing.

---
+++ Eddie/Action5.py
@@ -0,0 +1,21 @@
+#!/user/bin/python
+"""
+Placeholder specific Action class.
+"""
+from Action import Action
+
+from globalVars import EXIT_CODE_DONE
+from globalVars import EXIT_CODE_ERROR
+from globalVars import EXIT_CODE_A5
+
+class Action5(Action):
+    def execute(self,loops = 50):
+        """
+        Main executing method of this Action.
+        @param loops: The amount of times the action will execute a "step" until it finishes. Defaults to 50.
+        """
+        self.max_loops = loops
+        if self.loopCheck() == EXIT_CODE_DONE:
+            return EXIT_CODE_DONE
+        print self.loops_executed
+        return EXIT_CODE_A5
