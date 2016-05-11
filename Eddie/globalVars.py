656af0d22674cec03fc6725a7675615225e4e054
Marien Wolthuis <marien@marienwolthuis.nl>
2016-05-11 16:52:34 +0200

    Added needed channels for Eddie

--- Eddie/globalVars.py
+++ Eddie/globalVars.py
@@ -5,7 +5,10 @@ Some global variables that should be kept centralised
 
 ## ZMQ channels used
 CHANNEL_TARGETDATA  = "tcp://127.0.0.1:4001" 
-CHANNEL_BEATDATA    = "tcp://127.0.0.1:4002"
+CHANNEL_BEATDATA    = "tcp://127.0.0.1:4002" # Not intended to be used for Eddie, but kept for Fritz
+CHANNEL_MOVEMENTDATA= "tcp://127.0.0.1:4003"
+CHANNEL_ENERGYDATA  = "tcp://127.0.0.1:4004"
+CHANNEL_MODE        = "tcp://127.0.0.1:4005"
 
 ## Arduinos
 BOT_ARDUINO_ADDRESS = '/dev/ttyACM3'
