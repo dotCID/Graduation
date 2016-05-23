5d1584c1157619813305fc96abc54855b0c2f314
Marien Wolthuis <marien@marienwolthuis.nl>
2016-05-23 12:13:29 +0200

    Detection now runs on InfraRed with hardware filter

--- Eddie/Sensors/simpleCV_2.py
+++ Eddie/Sensors/simpleCV_2.py
@@ -1,9 +1,14 @@
+#!/user/bin/python
 '''
 This script uses SimpleCV to find a blue LED and report its position relative to the camera view centre and whether it is inside the target area.
 
 Attempted stabilisation of the output by tracking a circular object instead and altering exposure of the camera.
 '''
-#!/user/bin/python
+
+# make it possible to import from parent directory:
+import sys
+sys.path.insert(0,'..')
+
 import time, math, SimpleCV
 import zmq, json
 import subprocess as sp
@@ -14,13 +19,13 @@ printing = True
 
 dpx = 0.0725 # approximate amount of degrees per pixel
 
-width = 640
-height = 480
+width = 1920
+height = 1080
 camera_id = 'video' + str(CAMERA_ID_NUMBER)
 
 # Adjust camera settings from OS, since SimpleCV's commands don't do anything:
 sp.call(["uvcdynctrl -d '"+camera_id+"' -s 'Exposure, Auto' 1"], shell = True)         # Disable auto exposure
-sp.call(["uvcdynctrl -d '"+camera_id+"' -s 'Exposure (Absolute)' 2"], shell = True)    # Set absolute exposure
+sp.call(["uvcdynctrl -d '"+camera_id+"' -s 'Exposure (Absolute)' 2000"], shell = True)    # Set absolute exposure
 
 display = SimpleCV.Display()
 cam = SimpleCV.Camera(CAMERA_ID_NUMBER, {"width":width,"height":height})
@@ -29,6 +34,7 @@ cam = SimpleCV.Camera(CAMERA_ID_NUMBER, {"width":width,"height":height})
 box_d = 20
 yTgt = (height/2-box_d, height/2+box_d)
 xTgt = (width/2-box_d, width/2+box_d)
+box_clr = SimpleCV.Color.RED
 
 centre = (height/2, width/2)
 
@@ -38,22 +44,20 @@ socket = context.socket(zmq.PUB)
 socket.bind(CHANNEL_TARGETDATA)
 
 
-def search(): # different target than simpleCV.py
+def search():
     img = cam.getImage()
-    objective = img.colorDistance(color=(40,70,255)).invert()
-    seg_objective = objective.stretch(160,255)
+    objective = img.colorDistance(color=(255,255,255)).invert()
+    seg_objective = objective.stretch(200,255)
     blobs = seg_objective.findBlobs()
     
     if blobs:
-        circles = blobs.filter([b.isCircle(0.75) for b in blobs])
-        if circles and  circles[-1].area() > 2:
-            center_point = (circles[-1].x, circles[-1].y)
-            img.drawCircle((circles[-1].x, circles[-1].y), 10,SimpleCV.Color.YELLOW,3)
-            
-            img.dl().rectangle2pts((xTgt[0], yTgt[0]), (xTgt[1],yTgt[1]), SimpleCV.Color.RED)
-            img.show()
-            return center_point
-    img.dl().rectangle2pts((xTgt[0], yTgt[0]), (xTgt[1],yTgt[1]), SimpleCV.Color.RED)
+        center_point = (blobs[-1].x, blobs[-1].y)
+        img.drawCircle((blobs[-1].x, blobs[-1].y), 10,SimpleCV.Color.YELLOW,3)
+        
+        img.dl().rectangle2pts((xTgt[0], yTgt[0]), (xTgt[1],yTgt[1]), box_clr)
+        img.show()
+        return center_point
+    img.dl().rectangle2pts((xTgt[0], yTgt[0]), (xTgt[1],yTgt[1]), box_clr)
     img.show()
     return None
 
@@ -97,6 +101,11 @@ while display.isNotDone():
         deg_y = tar_y * dpx
     else:
         deg_y = 0
+        
+    if deg_y is 0 and deg_x is 0 and found:
+        box_clr = SimpleCV.Color.GREEN
+    else:
+        box_clr = SimpleCV.Color.RED
     
     #output the data
     # not needed if there's no new data to report
