ceebf894a690bb4a0eb85ab6104dd1b68845af96
Marien Wolthuis <marien@marienwolthuis.nl>
2016-05-28 17:46:57 +0200

    Tested some things, so this file changed.

--- Eddie/test_script.py
+++ Eddie/test_script.py
@@ -2,14 +2,22 @@
 """
 Short script to test the functionality of code.
 """
-from Actions import Action_Scan
-from Actions import Action_Contact
 from Actions import Action
+from Actions import Action_Contact
+from Actions import Action_Scan
+from Actions import Action_Focus
+from Actions import Action_Stevie
+from Actions import Action_Bored
+
 import time
-contact = Action_Contact.SpecificAction()
-scan = Action_Scan.SpecificAction()
+
 a = Action.Action()
+contact     = Action_Contact.SpecificAction()
+scan        = Action_Scan.SpecificAction()
+focus       = Action_Focus.SpecificAction()
+stevie      = Action_Stevie.SpecificAction()
+bored       = Action_Bored.SpecificAction()
 
 while True:
-    scan.execute()
+    bored.execute()
 
