04f01c10b96c2517c21e4356f7b90b0699c9242e
Marien Wolthuis <marien@marienwolthuis.nl>
2016-05-29 14:27:44 +0200

    Moved IMU contact to interface and added BPM detection.

--- Eddie/Sensors/beatDetect.py
+++ Eddie/Sensors/beatDetect.py
@@ -27,6 +27,10 @@
 # Adapted for use during my graduation project. 
 # Most additions/alterations are marked with "MCW"
 
+# MCW: make it possible to import from parent directory:
+import sys
+sys.path.insert(0,'..')
+
 import sys
 import numpy
 import numpy.linalg
@@ -35,10 +39,8 @@ from datetime import datetime
 from time import sleep
 
 from globalVars import CHANNEL_BEATDATA
-from globalVars import IMU_ARDUINO_ADDRESS
-from globalVars import IMU_ARDUINO_BAUDRATE
-import serial, time
-import zmq
+from globalVars import CHANNEL_IMU_RAWACCEL
+import time, zmq
 
 # Configuration: How often should we re-estimate the sample
 # frequency (based on how fast data is coming in, as long
@@ -72,189 +74,210 @@ history_phase = [0.0 for i in xrange(10)] # computed phases
 phase_at_zero = 0
 last_beat = 0
 
-# MCW: Set up the Arduino
-imu_arduino = serial.Serial(IMU_ARDUINO_ADDRESS, IMU_ARDUINO_BAUDRATE, timeout=.1)
-line = imu_arduino.readline().strip()
-print "imu: \n"
-print line
-
 # MCW: Set up ZMQ publishing:
 context = zmq.Context()
 socket = context.socket(zmq.PUB)
 socket.bind(CHANNEL_BEATDATA)
 
-# MCW: Function to read the imu arduino and check if the line is new
-# TODO: move this to the interface?
-def ardRead():
-    global line, imu_arduino
-    newline = imu_arduino.readline().strip()
-    if newline != line: 
-        line = newline
-    return line
-    
+## ZMQ Raw Accel channel - Raw accelleration from the IMU
+ra_context = zmq.Context()
+rawAccelChannel = ra_context.socket(zmq.SUB)
+rawAccelChannel.setsockopt(zmq.CONFLATE, 1 )
+rawAccelChannel.setsockopt(zmq.SUBSCRIBE, '')
+rawAccelChannel.connect(CHANNEL_IMU_RAWACCEL)
+
+accelPoller = zmq.Poller()
+accelPoller.register(rawAccelChannel, zmq.POLLIN)
+
 # MCW: Arduino-style millis() function for timekeeping
 millis = lambda: int(round(time.time() * 1000))
 
+# MCW: Simple BPM tracking list
+BPM = 0.0
+beatTimes = range(10)
+
+# MCW: Threshold above which beats 'count':
+S_THRESHOLD = 1.0
+
+# MCW: BPM computation
+def computeBPM():
+    '''
+    @author MCW
+    Takes the average BPM over n-1 time stamps
+    '''
+    global beatTimes
+    diff = range(len(beatTimes)-1)
+    for i in range(len(beatTimes)-1):
+        diff[i] = beatTimes[i+1] - beatTimes[i]
+    
+    t = sum(diff)
+    
+    if t != 0:
+        avgDiff = t / float(len(diff))
+        _bpm = 60000.0 / avgDiff
+    else:
+        _bpm = 0
+    
+    return _bpm
+    
+def listShift(ls, newEntry):
+    '''
+    @author MCW
+    Shifts the contents of a list and adds the new entry to the end
+    '''
+    for i in range(len(ls)-1):
+        ls[i] = ls[i+1]
+    ls[-1] = newEntry
+    
+    return ls
+        
+
 print "--------------\nBeat.py is now running.\n--------------\n"
 
-while True:
-    # MCW: Read from the device.
-    arduino_data = ardRead()
-    
-    # MCW: Testing with the imu script, all useful data starts with a ">" character
-    # because not all output can be controlled. The rest is for calibration etc.
-    if len(arduino_data) <= 1:
-        # garbage input
-        sleep(0.02)
-        continue
-    elif arduino_data[0] != ">":
-        # input not relevant for this script
-        print arduino_data
-        sleep(0.02)
-        continue
-    else:
-        # strip the ">"
-        arduino_data = arduino_data[1:]
-    
-    # MCW: Split the data
-    line = arduino_data.split(",")
-    
-    # Get the accelerometer data and put it into a history array.
-    vector = [float(f) for f in line[0:3]]
-    history_acc.append(vector)
-    if len(history_acc) > sample_history_size: history_acc.pop(0)
-    
-    # Estimate the sample frequency every 10 samples by the
-    # number of samples acquired divided by the time elapsed.
-    # Make sure that this is executed on the first iteration.
-    if (current_sample_num % sample_time_every) == 0:
-        history_time.append(datetime.now())
-        if len(history_time) > 10: history_time.pop(0)
-        ts = (history_time[-1]-history_time[0]).total_seconds()
-        if ts == 0.0: continue
-        sample_freq = sample_time_every * float(len(history_time)) / ts
-    current_sample_num += 1
-    if sample_freq == 0.0: continue # not yet computed
+while True:    
 
-    # There must be something like a 3D FFT, but to use the
-    # normal FFT we must compute a single value, not a vector,
-    # at each time in the history. We need a method to convert
-    # the 3D history data into 1D data.
+    # read from ZMQ; only continue if there is new data to process
+    if len(accelPoller.poll(0)) is not 0:
+        rawAccelData = rawAccelChannel.recv_json()
     
-    # Let's assume that the device is moving back and forth
-    # along a line in 3D space (i.e. not a circular motion).
-    # Then the best history to pass into the FFT will be the
-    # devices position (accelerometer reading) along that line,
-    # i.e. a projection of the position vector onto the line.
-    # Call that line the primary direction of motion.
-    
-    # To compute the primary direction of motion, we will use
-    # my friend the Singular Value Decomposition, which is a
-    # technique for dimensionality reduction (e.g. 3D to 1D).
-    # We pass the SVD the history array, and it computes three
-    # (because the original vectors are 3D) new vectors that
-    # when linearly combined best approximate the original
-    # matrix of history vectors. The new vectors are ordered,
-    # and the first is the vector that explains most of the
-    # original history matrix. Thus, it is the primary direction
-    # of motion.
-    
-    # In the language of FFTs, there is a DC component to the
-    # accelerometer data. At rest, the accelerometer reports the
-    # force of gravity. We must subtract the DC component before
-    # using SVD, or else it will get in the way.
-    #
-    # Subtract off the mean value on each axis from the history.
-    m = [0, 0, 0]
-    for i in xrange(3): m[i] = numpy.mean([h[i] for h in history_acc])
-    m = numpy.array(m)
-    history = [h-m for h in history_acc]
-    
-    # Compute the SVD.
-    u, s, vT = numpy.linalg.svd(history)
-    
-    # vT[0]  - the primary direction of motion (vector norm is 1.0)
-    # u[:,0] - the coordinate on the 'primary direction of motion'
-    #          axis for each point in the history (vector norm is 1.0)
-    # s[0]   - a scale factor that when applied to u and vT gives back
-    #          the actual magnitudes of the acceleration over the history
-    #          (so it is a sort of average absolute acceleration over
-    #          the history).
-    
-    history_acc1d = u[:,0]
-    
-    # Compute the fundamental frequency of the 1D history data by passing
-    # the 1D accelerometer values into a Fast Fourier Transform. This
-    # gives a power level at a range of frequency components in the signal.
-    FFT = scipy.fft(history_acc1d)
-    
-    # The FFT power values are complex numbers at first. Take abs to get
-    # a real power. Also, weight low frequencies more highly since there
-    # may be high-frequency energy especially at multiples of the fundamental
-    # frequency. This is a made up exponential decay that seems to work well.
-    FFTpower = abs(FFT)
-    for i in xrange(len(FFT)):
-        FFTpower[i] *= 2**(-i/float(len(history_acc1d)))
-    
-    # Find the peak of the power spectrum, and the frequency that corresponds
-    # to that power.
-    f0_i = numpy.argmax(FFTpower[1:]) + 1 # skip the DC component of the signal
-    freqs = scipy.fftpack.fftfreq(len(history_acc1d), 1.0/sample_freq)
-    f0 = abs(freqs[f0_i]) # FFT gives negative frequencies? Hmmm.
-    
-    # Add the computed frequency to a short history and smooth the frequency
-    # by taking the mean over the history.
-    history_f0.append(f0)
-    history_f0.pop(0)
-    f0 = numpy.mean(history_f0)
-    
-    # Print the fundamental frequency and the 'average' acceleration.
-    #print f0, s[0]
-    
-    # MCW: changed above to zmq JSON
-    msg = {
-                't'   : millis(),
-                'f0'  : f0,
-                's0'  : s[0],
-                'beat': False
-            }
-    
-    samples_per_beat = int(round(sample_freq / f0))
-    if (current_sample_num % beat_phase_every) == 0:
-        # We can also predict the next beat by getting the phase of the
-        # beat at the current time and projecting forward based on the
-        # computed period, i.e. the inverse of the fundamental frequency.
-    
-        # This gets the current phase in radians in the range [-pi,pi].
-        phase = numpy.angle(FFT[f0_i])
+        # Convert JSON to list and lose the 't'
+        line = [ rawAccelData['x'], rawAccelData['y'], rawAccelData['z'] ]
         
-        # If there are X samples in a beat, what phase was sample 0
-        # in the range of [0,X)?
-        phase_now = int(round( (phase + numpy.pi/2)/(2*numpy.pi) * samples_per_beat ))
-        phase_at_zero = (phase_now - current_sample_num) % samples_per_beat
+        # Get the accelerometer data and put it into a history array.
+        vector = [float(f) for f in line[0:3]]
+        history_acc.append(vector)
+        if len(history_acc) > sample_history_size: history_acc.pop(0)
         
-        # The phase is very wobbly over time, so we need to smooth it.
-        history_phase.append(phase_at_zero)
-        history_phase.pop(0)
-        phase_at_zero = int(numpy.median(history_phase))
-    
-    # Should we beat now? Just prevent two beats close together.
-    if samples_per_beat is not 0:
-        if (current_sample_num % samples_per_beat) == 0 \
-          and current_sample_num > last_beat + .8*samples_per_beat:
-            msg = {
-                't'   : millis(),
-                'f0'  : f0,
-                's0'  : s[0],
-                'beat': True
-            }
-            last_beat = current_sample_num
-    
-    # MCW: Filter out idle beats (at least from output:
-    if s[0] < 1:
-        msg['beat'] = False
-    
-    # MCW: Send message
-    print "Sent beatData: ", 
-    print msg
-    socket.send_json(msg)
+        # Estimate the sample frequency every 10 samples by the
+        # number of samples acquired divided by the time elapsed.
+        # Make sure that this is executed on the first iteration.
+        if (current_sample_num % sample_time_every) == 0:
+            history_time.append(datetime.now())
+            if len(history_time) > 10: history_time.pop(0)
+            ts = (history_time[-1]-history_time[0]).total_seconds()
+            if ts == 0.0: continue
+            sample_freq = sample_time_every * float(len(history_time)) / ts
+        current_sample_num += 1
+        if sample_freq == 0.0: continue # not yet computed
+
+        # There must be something like a 3D FFT, but to use the
+        # normal FFT we must compute a single value, not a vector,
+        # at each time in the history. We need a method to convert
+        # the 3D history data into 1D data.
+        
+        # Let's assume that the device is moving back and forth
+        # along a line in 3D space (i.e. not a circular motion).
+        # Then the best history to pass into the FFT will be the
+        # devices position (accelerometer reading) along that line,
+        # i.e. a projection of the position vector onto the line.
+        # Call that line the primary direction of motion.
+        
+        # To compute the primary direction of motion, we will use
+        # my friend the Singular Value Decomposition, which is a
+        # technique for dimensionality reduction (e.g. 3D to 1D).
+        # We pass the SVD the history array, and it computes three
+        # (because the original vectors are 3D) new vectors that
+        # when linearly combined best approximate the original
+        # matrix of history vectors. The new vectors are ordered,
+        # and the first is the vector that explains most of the
+        # original history matrix. Thus, it is the primary direction
+        # of motion.
+        
+        # In the language of FFTs, there is a DC component to the
+        # accelerometer data. At rest, the accelerometer reports the
+        # force of gravity. We must subtract the DC component before
+        # using SVD, or else it will get in the way.
+        #
+        # Subtract off the mean value on each axis from the history.
+        m = [0, 0, 0]
+        for i in xrange(3): m[i] = numpy.mean([h[i] for h in history_acc])
+        m = numpy.array(m)
+        history = [h-m for h in history_acc]
+        
+        # Compute the SVD.
+        u, s, vT = numpy.linalg.svd(history)
+        
+        # vT[0]  - the primary direction of motion (vector norm is 1.0)
+        # u[:,0] - the coordinate on the 'primary direction of motion'
+        #          axis for each point in the history (vector norm is 1.0)
+        # s[0]   - a scale factor that when applied to u and vT gives back
+        #          the actual magnitudes of the acceleration over the history
+        #          (so it is a sort of average absolute acceleration over
+        #          the history).
+        
+        history_acc1d = u[:,0]
+        
+        # Compute the fundamental frequency of the 1D history data by passing
+        # the 1D accelerometer values into a Fast Fourier Transform. This
+        # gives a power level at a range of frequency components in the signal.
+        FFT = scipy.fft(history_acc1d)
+        
+        # The FFT power values are complex numbers at first. Take abs to get
+        # a real power. Also, weight low frequencies more highly since there
+        # may be high-frequency energy especially at multiples of the fundamental
+        # frequency. This is a made up exponential decay that seems to work well.
+        FFTpower = abs(FFT)
+        for i in xrange(len(FFT)):
+            FFTpower[i] *= 2**(-i/float(len(history_acc1d)))
+        
+        # Find the peak of the power spectrum, and the frequency that corresponds
+        # to that power.
+        f0_i = numpy.argmax(FFTpower[1:]) + 1 # skip the DC component of the signal
+        freqs = scipy.fftpack.fftfreq(len(history_acc1d), 1.0/sample_freq)
+        f0 = abs(freqs[f0_i]) # FFT gives negative frequencies? Hmmm.
+        
+        # Add the computed frequency to a short history and smooth the frequency
+        # by taking the mean over the history.
+        history_f0.append(f0)
+        history_f0.pop(0)
+        f0 = numpy.mean(history_f0)
+        
+        msg = {
+                    't'   : millis(),
+                    'f0'  : f0,
+                    's0'  : s[0],
+                    'bpm' : BPM,
+                    'beat': False
+                }
+        
+        samples_per_beat = int(round(sample_freq / f0))
+        if (current_sample_num % beat_phase_every) == 0:
+            # We can also predict the next beat by getting the phase of the
+            # beat at the current time and projecting forward based on the
+            # computed period, i.e. the inverse of the fundamental frequency.
+        
+            # This gets the current phase in radians in the range [-pi,pi].
+            phase = numpy.angle(FFT[f0_i])
+            
+            # If there are X samples in a beat, what phase was sample 0
+            # in the range of [0,X)?
+            phase_now = int(round( (phase + numpy.pi/2)/(2*numpy.pi) * samples_per_beat ))
+            phase_at_zero = (phase_now - current_sample_num) % samples_per_beat
+            
+            # The phase is very wobbly over time, so we need to smooth it.
+            history_phase.append(phase_at_zero)
+            history_phase.pop(0)
+            phase_at_zero = int(numpy.median(history_phase))
+        
+        # Should we beat now? Just prevent two beats close together.
+        if samples_per_beat is not 0:
+            if (current_sample_num % samples_per_beat) == 0 \
+              and current_sample_num > last_beat + .8*samples_per_beat \
+              and s[0] > S_THRESHOLD:
+
+                beatTimes = listShift(beatTimes, millis())
+                BPM = computeBPM()            
+                            
+                msg = {
+                    't'   : millis(),
+                    'f0'  : f0,
+                    's0'  : s[0],
+                    'bpm' : BPM,
+                    'beat': True
+                }
+                last_beat = current_sample_num
+            
+        # MCW: Send message
+        print "Sent beatData: ", 
+        print msg
+        socket.send_json(msg)
