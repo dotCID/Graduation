4a8267419bc0b791c0652be5f36c8fd9e230d7ea
Marien Wolthuis <marien@marienwolthuis.nl>
2016-05-29 14:27:09 +0200

    MovementData renamed to IMU_RAWPOS

--- Eddie/globalVars.py
+++ Eddie/globalVars.py
@@ -6,16 +6,18 @@ Some global variables that should be kept centralised
 ## ZMQ channels used
 CHANNEL_TARGETDATA  = "tcp://127.0.0.1:4001" 
 CHANNEL_BEATDATA    = "tcp://127.0.0.1:4002" # Not intended to be used for Eddie, but kept for Fritz
-CHANNEL_MOVEMENTDATA= "tcp://127.0.0.1:4003"
 CHANNEL_ENERGYDATA  = "tcp://127.0.0.1:4004"
 CHANNEL_MODE        = "tcp://127.0.0.1:4005"
 CHANNEL_BPM         = "tcp://127.0.0.1:4006"
+
+CHANNEL_IMU_RAWACCEL= "tcp://127.0.0.1:4007"
+CHANNEL_IMU_RAWPOS  = "tcp://127.0.0.1:4008"
 
 ## Arduinos
 BOT_ARDUINO_ADDRESS = '/dev/ttyUSB0'
 BOT_ARDUINO_BAUDRATE= 115200
 
-IMU_ARDUINO_ADDRESS = '/dev/ttyUSB1'
+IMU_ARDUINO_ADDRESS = '/dev/ttyUSB0'
 IMU_ARDUINO_BAUDRATE= 115200
 
 CONTROLBOX_ARDUINO_ADDRESS = '/dev/ttyACM3'
