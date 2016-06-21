907403b19ff7d6d75baafd8c0962bf429af46ce6
Marien Wolthuis <marien@marienwolthuis.nl>
2016-06-21 11:45:37 +0200

    Stability improvement

--- Fritz/Arduino Firmware/pedal_arduino.ino
+++ Fritz/Arduino Firmware/pedal_arduino.ino
@@ -1,13 +1,22 @@
 void setup() {
-  // put your setup code here, to run once:
   pinMode(13, OUTPUT);
+  // To improve stability, some delaying loop
+  for(int i=0;i<3;i++){
+    digitalWrite(13, LOW);
+    delay(500);
+    digitalWrite(13, HIGH);
+    delay(500);
+  }
+  
   pinMode(11, INPUT_PULLUP);
   pinMode(10, INPUT_PULLUP);
   Serial.begin(115200);
+  delay(500);
+  Serial.println("Setup complete.");
+  delay(500);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   bool p10State = digitalRead(10);
   bool p11State = digitalRead(11);
   digitalWrite(13, p10State);
