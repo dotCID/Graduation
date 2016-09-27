1d0216a8dc8db09ee0d23bdfce56dd1cccf0b404
Marien Wolthuis <marien@marienwolthuis.nl>
2016-09-27 17:02:04 +0200

    Changes to support LEDs etc

--- Giorgio/Arduino Firmware/Bot_arduino.ino
+++ Giorgio/Arduino Firmware/Bot_arduino.ino
@@ -23,17 +23,20 @@
 #define TV_ID  3
 
 // Pulse lengths for "towar dpro MG996R"
-#define SERVOMIN  125 // this is the 'minimum' pulse length count (out of 4096)
-#define SERVOMAX  525 // this is the 'maximum' pulse length count (out of 4096)
+#define TWP_MIN  125 // this is the 'minimum' pulse length count (out of 4096)
+#define TWP_MAX  525 // this is the 'maximum' pulse length count (out of 4096)
+
+#define FTK_MIN  125
+#define FTK_MAX  650
 
 // Min, max and default values for the servos
 #define BH_MIN 0
 #define BH_MAX 180
-#define BH_DEF 170
+#define BH_DEF 67
 
 #define BV_MIN 0
 #define BV_MAX 180
-#define BV_DEF 30
+#define BV_DEF 50
 
 #define TV_MIN 0
 #define TV_MAX 180
@@ -45,21 +48,21 @@ Adafruit_PWMServoDriver s_drv = Adafruit_PWMServoDriver();
 Adafruit_NeoPixel pixelstrip = Adafruit_NeoPixel(NUMPIXELS, NPX_PIN, NEO_GRB + NEO_KHZ800);
 
 double BH_pos,BV_pos, TV_pos;
-bool fullPrint = false;
-bool fullStop = false;
+bool fullPrint = true;
+bool fullStop = false; // if true, all servos initiate to 90 degrees by default instead of the above defaults
 
 uint32_t red    = pixelstrip.Color(255,   0,   0);
 uint32_t green  = pixelstrip.Color(  0, 255,   0);
 uint32_t blue   = pixelstrip.Color(  0,   0, 255);
 uint32_t white  = pixelstrip.Color(255, 255, 255);
-uint32_t purple = pixelstrip.Color(138,  43, 226);
+uint32_t purple = pixelstrip.Color(138,   0, 226);
 uint32_t none   = pixelstrip.Color(  0,   0,   0);
 
 uint32_t colours[] = { red,  white, red, green, blue};
 
 int npx_currentPattern = PXPAT_PULSE;
 unsigned long npx_timer = millis();
-uint16_t npx_pulseDelay = 100;
+uint16_t npx_pulseDelay = 300;
 
 String inputString ="";
 bool stringComplete = false;
@@ -72,6 +75,12 @@ void setup(){
     }
     pixelstrip.show();
     
+    // Glow when starting
+    for(int i=0;i<NUMPIXELS;i++){
+            pixelstrip.setPixelColor(i, pixelstrip.Color(100, 100, 75));
+    }
+    pixelstrip.show();
+    
     Serial.begin(115200);
     
     s_drv.begin();  
@@ -81,12 +90,10 @@ void setup(){
     BV_pos = BV_DEF;
     TV_pos = TV_DEF;
     
+    delay(1000);
+    
     Serial.println(F("Setup done."));
-    // Glow when done
-    for(int i=0;i<NUMPIXELS;i++){
-            pixelstrip.setPixelColor(i, pixelstrip.Color(100, 100, 75));
-    }
-    pixelstrip.show();
+    
 }
 
 void loop(){
@@ -118,45 +125,59 @@ void readSerial(){
   }
   
   if(stringComplete) {
-      if(inputString == "stop\n"){
+      if(inputString.startsWith("stop")){
           Serial.println(F("Stopping."));
           fullStop = true;
-    }else if(inputString == "start\n"){
+    }else if(inputString.startsWith("start")){
           Serial.println(F("Starting."));
           fullStop = false;
-    }else if(inputString == "home\n"){
+    }else if(inputString.startsWith("home")){
         Serial.println(F("Returning to default angles."));
         BH_pos = BH_DEF;
         BV_pos = BV_DEF;
         TV_pos = TV_DEF;
-    }else if(inputString == "on\n"){
+    }else if(inputString.indexOf("on")!=-1){
       Serial.println(F("LED on"));
         digitalWrite(13,HIGH);
-    }else if(inputString == "off\n"){
+    }else if(inputString.indexOf("off")!=-1){
       Serial.println(F("LED off"));
         digitalWrite(13,LOW);
-    }if(inputString.startsWith("BH")){
+    }else if(inputString.startsWith("BH")){
       String val = inputString.substring(3,inputString.length());
       BH_pos = val.toFloat();
       if(BH_pos<BH_MIN) BH_pos = BH_MIN;
       if(BH_pos>BH_MAX) BH_pos = BH_MAX;
       
       if(fullPrint) Serial.print(F("BH_pos is now "));Serial.println(BH_pos);
-    }if(inputString.startsWith("BV")){
+      
+     if(fullPrint) Serial.print("Set BH to ");
+     if(fullPrint) Serial.print((int)degree2Pulse(BH_pos, "TWP"));
+    }else if(inputString.startsWith("BV")){
       String val = inputString.substring(3,inputString.length());
       BV_pos = val.toFloat();
       if(BV_pos<BV_MIN) BV_pos = BV_MIN;
       if(BV_pos>BV_MAX) BV_pos = BV_MAX;
       
       if(fullPrint) Serial.print(F("BV_pos is now "));Serial.println(BV_pos);
-    }if(inputString.startsWith("TV")){
+      
+      
+      if(fullPrint) {   Serial.print("Set BV to ");
+                        Serial.print((int)degree2Pulse(BV_pos, "FTK")); Serial.print(", ");
+                        Serial.println((int)degree2Pulse(180.0-BV_pos, "FTK"));
+                    }
+      
+    }else if(inputString.startsWith("TV")){
       String val = inputString.substring(3,inputString.length());
       TV_pos = val.toFloat();
       if(TV_pos<TV_MIN) TV_pos = TV_MIN;
       if(TV_pos>TV_MAX) TV_pos = TV_MAX;
       
       if(fullPrint) Serial.print(F("TV_pos is now "));Serial.println(TV_pos);
+    }else if(inputString.indexOf("undo")!=-1){
+        inputString = "";
+        Serial.println(F("Cleared input string."));
     }
+    inputString = "";
   }
 }
 
@@ -166,16 +187,17 @@ void readSerial(){
 void runMotors(){
     if(!fullStop){
         if((BH_pos >= BH_MIN) && (BH_pos <= BH_MAX)){
-            s_drv.setPWM(BH_ID, 0, degree2Pulse(BH_pos));
+            s_drv.setPWM(BH_ID, 0, degree2Pulse(BH_pos, "TWP"));
         }
         
         if((BV_pos >= BV_MIN) && (BV_pos <= BV_MAX)){
-            s_drv.setPWM(BV1_ID, 0, degree2Pulse(BV_pos));
-            s_drv.setPWM(BV2_ID, 0, degree2Pulse(180.0 - BV_pos)); // control second motor inversely
+            s_drv.setPWM(BV1_ID, 0, degree2Pulse(BV_pos, "FTK"));
+            s_drv.setPWM(BV2_ID, 0, degree2Pulse(180.0 - BV_pos, "FTK")); // control second motor inversely
+            
         }
         
         if((TV_pos >= TV_MIN) && (TV_pos <= TV_MAX)){
-            s_drv.setPWM(TV_ID, 0, degree2Pulse(TV_pos));
+            s_drv.setPWM(TV_ID, 0, degree2Pulse(TV_pos, "TWP"));
         }
     }else{
         //Serial.print(F("All motion is currently stopped. Please send \"start\" to start moving."));
@@ -185,8 +207,9 @@ void runMotors(){
 /* 
     Converts positions in degrees to pulsewidth signals for the driver 
 */
-uint16_t degree2Pulse(int degree){
-    return (uint16_t) map(degree, 0.0, 180.0, SERVOMIN, SERVOMAX);
+uint16_t degree2Pulse(double degree, char* type){
+    if(type == "TWP")  return (uint16_t) map(degree, 0.0, 180.0, TWP_MIN, TWP_MAX);
+    if(type == "FTK")  return (uint16_t) map(degree, 0.0, 180.0, FTK_MIN, FTK_MAX);
 }
 
 /*
@@ -195,71 +218,79 @@ uint16_t degree2Pulse(int degree){
 bool npx_lit = false;
 int npx_i = 0;
 void ledPattern(int pattern){
-    if(pattern == PXPAT_PULSE){
-        if(millis() - npx_timer > npx_pulseDelay){
-            if(npx_lit){
-                // turn all off
-                for(int i=0;i<NUMPIXELS;i++){
-                    pixelstrip.setPixelColor(i, none);
+    if(!fullStop){
+        if(pattern == PXPAT_PULSE){
+            if(millis() - npx_timer > npx_pulseDelay){
+                if(npx_lit){
+                    // turn all off
+                    for(int i=0;i<NUMPIXELS;i++){
+                        pixelstrip.setPixelColor(i, none);
+                    }
+                }else{
+                    for(int i=0;i<NUMPIXELS;i++){
+                        pixelstrip.setPixelColor(i, purple);
+                    }
+                }
+                
+                pixelstrip.show();
+                npx_lit = !npx_lit;
+                npx_timer = millis();
+            }
+        }else if(pattern == PXPAT_BPMUP){
+            if(npx_i<PXGROUP){
+                if(millis() - npx_timer > npx_pulseDelay){
+                    // turn all off
+                    for(int j=0;j<NUMPIXELS;j++){
+                        pixelstrip.setPixelColor(j, none);
+                    }
+                  
+                    // turn on every 5th one
+                    for(int j=0;j<NUMPIXELS/PXGROUP;j++){
+                      if(j!=1){
+                        pixelstrip.setPixelColor(PXGROUP*2-npx_i-1, blue);
+                      }else{
+                        pixelstrip.setPixelColor(npx_i+PXGROUP*j, blue);
+                      }
+                    }
+                    
+                    pixelstrip.show();
+                    npx_i++;
+                    npx_timer = millis();
                 }
             }else{
-                for(int i=0;i<NUMPIXELS;i++){
-                    pixelstrip.setPixelColor(i, purple);
-                }
+                npx_i = 0;
             }
-            
-            pixelstrip.show();
-            npx_lit = !npx_lit;
-            npx_timer = millis();
-        }
-    }else if(pattern == PXPAT_BPMUP){
-        if(npx_i<PXGROUP){
-            if(millis() - npx_timer > npx_pulseDelay){
-                // turn all off
-                for(int j=0;j<NUMPIXELS;j++){
-                    pixelstrip.setPixelColor(j, none);
+        }else if(pattern == PXPAT_BPMDW){
+            if(npx_i<PXGROUP){
+                if(millis() - npx_timer > npx_pulseDelay){
+                    // turn all off
+                    for(int j=0;j<NUMPIXELS;j++){
+                        pixelstrip.setPixelColor(j, none);
+                    }
+                  
+                    // turn on every 5th one
+                    for(int j=0;j<NUMPIXELS/PXGROUP;j++){
+                      if(j!=1){
+                        pixelstrip.setPixelColor(npx_i+PXGROUP*j, green);
+                      }else{
+                        pixelstrip.setPixelColor(PXGROUP*2-npx_i-1, green);
+                      }
+                    }
+                    
+                    pixelstrip.show();
+                    npx_i++;
+                    npx_timer = millis();
                 }
-              
-                // turn on every 5th one
-                for(int j=0;j<NUMPIXELS/PXGROUP;j++){
-                  if(j!=1){
-                    pixelstrip.setPixelColor(PXGROUP*2-npx_i-1, red);
-                  }else{
-                    pixelstrip.setPixelColor(npx_i+PXGROUP*j, red);
-                  }
-                }
-                
-                pixelstrip.show();
-                npx_i++;
-                npx_timer = millis();
+            }else{
+                npx_i = 0;
             }
-        }else{
-            npx_i = 0;
         }
-    }else if(pattern == PXPAT_BPMDW){
-        if(npx_i<PXGROUP){
-            if(millis() - npx_timer > npx_pulseDelay){
-                // turn all off
-                for(int j=0;j<NUMPIXELS;j++){
-                    pixelstrip.setPixelColor(j, none);
-                }
-              
-                // turn on every 5th one
-                for(int j=0;j<NUMPIXELS/PXGROUP;j++){
-                  if(j!=1){
-                    pixelstrip.setPixelColor(npx_i+PXGROUP*j, red);
-                  }else{
-                    pixelstrip.setPixelColor(PXGROUP*2-npx_i-1, red);
-                  }
-                }
-                
-                pixelstrip.show();
-                npx_i++;
-                npx_timer = millis();
-            }
-        }else{
-            npx_i = 0;
+        
+    }else{
+        for(int i=0;i<NUMPIXELS;i++){
+            pixelstrip.setPixelColor(i, red);
         }
+        pixelstrip.show();
     }
 }
 
