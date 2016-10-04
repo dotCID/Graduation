b20fc3c212c6a478606cfa5cf62e0fafa3cdf0c1
Marien Wolthuis <marien@marienwolthuis.nl>
2016-10-04 12:03:29 +0200

    Changed folder layout for easier reading by others

--- Charles/v2/20160413_controller.py
+++
@@ -1,6 +0,0 @@
-#simply attempts to keep the simpleCV script running at all times
-import RunCmd
-
-while True:
-    RunCmd.RunCmd(["python", "./20160413_simpleCV.py"], 3600).Run()
-
--- Eddie/Sensors/SCVcontroller.py
+++
@@ -1,6 +0,0 @@
-#simply attempts to keep the simpleCV script running at all times
-import RunCmd
-
-while True:
-    RunCmd.RunCmd(["python", "./simpleCV_3.py"], 600).Run()
-
